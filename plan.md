66bdfa7fe81d23392a2b4b277476ca247e38ec61
Adam Gray <adamgrayscale@gmail.com>
2024-12-04 13:58:26 -0600

    use web audio api to play audio with gain

---
+++ plan.md
@@ -0,0 +1,14 @@
+- load an audio track
+- display waveform
+- select x number of sections from audio
+- display track name, artist, and year
+- color code waveform sections
+- play audio, starting from section
+- multiple audio tracks playing at the same time (scary)
+- sequence playing sections in sync
+
+
+GOOD EXAMPLE
+https://www.youtube.com/watch?v=SBsRzyQ-TfM
+
+- how are we going to load data? idk
--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+
+const audioContext = new AudioContext();
+const gainNode = audioContext.createGain();
 
 function App() {
+  const trackRef = useRef(null);
+  const audioRef = useRef(null);
+  const [volume, setVolume] = useState(1);
+
+  useEffect(() => {
+    if (trackRef.current) return;
+    trackRef.current = audioContext.createMediaElementSource(audioRef.current);
+    trackRef.current.connect(gainNode).connect(audioContext.destination);
+  }, [audioRef]);
+
+  useEffect(() => {
+    gainNode.gain.value = parseFloat(volume);
+  }, [volume]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <audio ref={audioRef} src="/sample.wav"></audio>
+
+      <input
+        type="range"
+        id="volume"
+        min="0"
+        max="4"
+        value={volume}
+        step="0.01"
+        onChange={(e) => {
+          setVolume(e.target.value);
+        }}
+      />
+
+      <div>volume {volume}</div>
+
+      <button
+        onClick={() => {
+          if (audioContext.state === "suspended") {
+            audioContext.resume();
+          }
+
+          if (audioContext.state === "running") {
+            audioContext.suspend();
+            audioRef.current.pause();
+          } else if (audioContext.state === "suspended") {
+            audioContext.resume();
+            audioRef.current.play();
+          }
+        }}
+      >
+        Play/Pause
+      </button>
     </div>
   );
 }
